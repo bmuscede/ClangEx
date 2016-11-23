acb403432d83e935f32e0bcae2901ad4404eff39
Bryan J Muscedere <Bryan Muscedere>
2016-11-23 16:29:16 -0500

    Updated Clean.sh to remove all CMake files.

--- Clean.sh
+++ Clean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rm -r ClangEx CMakeFiles/ CMakeCache.txt cmake_install.cmake Makefile
+rm -r cmake-build-debug/ ClangEx ClangEx.cbp CMakeFiles/ CMakeCache.txt cmake_install.cmake Makefile
