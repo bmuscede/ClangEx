387e0da6e730ace0173a70dbb8b3ad8d4bac1ad2
Bryan J Muscedere <Bryan Muscedere>
2016-11-25 19:41:31 -0500

    Updated ClangEx schema to address new attributes.

--- Graph/TAGraph.h
+++ Graph/TAGraph.h
@@ -43,22 +43,22 @@ private:
     std::vector<ClangNode*> nodeList;
     std::vector<ClangEdge*> edgeList;
 
-    //TODO: Alter this to reflect ClangEx schema.
-    std::string const TA_SCHEMA = "//Generated TA File\n//Author: Jingwei Wu & Bryan J Muscedere\n\nSCHEME TUPLE :\n//N"
-            "odes\n$INHERIT\tcArchitecturalNds\tcRoot\n$INHERIT\tcAsgNds\t\t\tcRoot\n$INHERIT\tcSubSystem\t\tcArchitect"
-            "uralNds\n$INHERIT\tcFile\t\t\tcArchitecturalNds\n$INHERIT\tcExecutable\t\tcFile\n$INHERIT\tcObjectFile\t\t"
-            "cFile\n$INHERIT\tcArchiveFile\t\tcFile\n$INHERIT\tcClass\t\t\tcAsgNds\n$INHERIT\tcFunction\t\tcAsgNds\n$IN"
-            "HERIT\tcObject\t\t\tcAsgNds\n\n//Relationships\ncontain\t\tcRoot\t\tcRoot\ncall\t\tcFunction\tcFunction\nr"
-            "eference\tcFunction\tcObject\n\nSCHEME ATTRIBUTE :\n$ENTITY {\n\tx\n\ty\n\twidth\n\theight\n\tlabel\n}\n\n"
-            "cRoot {\n\telision = contain\n\tcolor = (0.0 0.0 0.0)\n\tfile\n\tline\n\tname\n}\n\ncAsgNds {\n\tbeg\n\ten"
-            "d\n\tfile\n\tline\n\tvalue\n\tcolor = (0.0 0.0 0.0)\n}\n\ncArchitecturalNds {\n\tclass_style = 4\n\tcolor "
-            "= (0.0 0.0 1.0)\n\tcolor = (0.0 0.0 0.0)\n}\n\ncSubSystem {\n\tclass_style = 4\n\tcolor = (0.0 0.0 1.0)\n}"
-            "\n\ncFile {\n\tclass_style = 2\n\tcolor = (0.9 0.9 0.9)\n\tlabelcolor = (0.0 0.0 0.0)\n}\n\ncExecutable {"
-            "\n\tclass_style = 4\n\tcolor = (0.8 0.9 0.9)\n\tlabelcolor = (0.0 0.0 0.0)\n}\n\ncObjectFile {\n\tclass_st"
-            "yle = 4\n\tcolor = (0.6 0.8 0.6)\n\tlabelcolor = (0.0 0.0 0.0)\n}\n\ncArchiveFile {\n\tclass_style = 4\n\t"
-            "color = (0.5 0.5 0.1)\n\tlabelcolor = (0.0 0.0 0.0)\n}\n\ncFunction {\n\tfilename\n\tcolor = (1.0 0.0 0.0)"
-            "\n\tlabelcolor = (0.0 0.0 0.0)\n}\n\ncObject {\n\tfilename\n}\n\ncClass {\n\tfilename\n\tcolor = (0.2 0.4 "
-            "0.1)\n\tlabelcolor = (0.0 0.0 0.0)\n}\n\n(reference) {\n\taccess\n}\n\n";
+    std::string const TA_SCHEMA = "//Generated TA File\n//Author: Jingwei Wu & Bryan J Muscedere\n\nSCHEME TUPLE :\n//No"
+            "des\n$INHERIT\tcArchitecturalNds\tcRoot\n$INHERIT\tcAsgNds\t\t\tcRoot\n$INHERIT\tcSubSystem\t\tcArchitectur"
+            "alNds\n$INHERIT\tcFile\t\t\tcArchitecturalNds\n$INHERIT\tcExecutable\t\tcFile\n$INHERIT\tcObjectFile\t\tcFi"
+            "le\n$INHERIT\tcArchiveFile\t\tcFile\n$INHERIT\tcClass\t\t\tcAsgNds\n$INHERIT\tcFunction\t\tcAsgNds\n$INHERI"
+            "T\tcObject\t\t\tcAsgNds\n\n//Relationships\ncontain\t\tcRoot\t\tcRoot\ncall\t\tcFunction\tcFunction\nrefere"
+            "nce\tcFunction\tcObject\ninherits\tcClass\t\tcClass\n\nSCHEME ATTRIBUTE :\n$ENTITY {\n\tx\n\ty\n\twidth\n\t"
+            "height\n\tlabel\n}\n\ncRoot {\n\telision = contain\n\tcolor = (0.0 0.0 0.0)\n\tfile\n\tline\n\tname\n}\n\nc"
+            "AsgNds {\n\tbeg\n\tend\n\tfile\n\tline\n\tvalue\n\tcolor = (0.0 0.0 0.0)\n}\n\ncArchitecturalNds {\n\tclass"
+            "_style = 4\n\tcolor = (0.0 0.0 1.0)\n\tcolor = (0.0 0.0 0.0)\n}\n\ncSubSystem {\n\tclass_style = 4\n\tcolor"
+            " = (0.0 0.0 1.0)\n}\n\ncFile {\n\tclass_style = 2\n\tcolor = (0.9 0.9 0.9)\n\tlabelcolor = (0.0 0.0 0.0)\n}"
+            "\n\ncExecutable {\n\tclass_style = 4\n\tcolor = (0.8 0.9 0.9)\n\tlabelcolor = (0.0 0.0 0.0)\n}\n\ncObjectFi"
+            "le {\n\tclass_style = 4\n\tcolor = (0.6 0.8 0.6)\n\tlabelcolor = (0.0 0.0 0.0)\n}\n\ncArchiveFile {\n\tclas"
+            "s_style = 4\n\tcolor = (0.5 0.5 0.1)\n\tlabelcolor = (0.0 0.0 0.0)\n}\n\ncFunction {\n\tfilename\n\tisStati"
+            "c\n\tvisibility\n\tcolor = (1.0 0.0 0.0)\n\tlabelcolor = (0.0 0.0 0.0)\n}\n\ncObject {\n\tfilename\n}\n\ncC"
+            "lass {\n\tfilename\n\tcolor = (0.2 0.4 0.1)\n\tlabelcolor = (0.0 0.0 0.0)\n}\n\n(reference) {\n\taccess\n}"
+            "\n\n";
 
     std::string generateInstances();
     std::string generateRelationships();
